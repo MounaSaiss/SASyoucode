31531c286fcaa42fb2a4b82e99e4db0cf1867e68
LunaDevX <mouna1.saiss@gmail.com>
2025-09-23 18:39:09 +0100

    all task commit

--- day1/task3.c
+++ day1/task3.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<math.h>
 int main (){
     float r,V;
     float pi=3.14159;
     printf("Veuillez saisir le  rayon de sphere:");
     scanf("%f",&r);
 
-    V=(4.0f/3.0f)*pi*pow(r,3);
+    V=(4.0f/3.0f)*pi*r*r*r;
 
     printf("le volume est :%f",V);
     return 0;
