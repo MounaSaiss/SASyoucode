ed1d3e610f3b13567a121db2586ab7a415f0c332
LunaDevX <mouna1.saiss@gmail.com>
2025-09-23 14:41:29 +0100

    task5

---
+++ day1/task5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+int main (){
+    float x,S;
+printf("Veuillez saisir la valeur de x :");
+scanf("%d",&x);
+S=3*(x*x*x*x*x)+2*(x*x*x*x)-5*(x*x*x)-(x*x)+7*x-6;
+printf("S=%f",S);
+    return 0;
+}
