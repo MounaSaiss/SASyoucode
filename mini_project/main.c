e236fd02adc507584f48757af88f5d79b213ddff
LunaDevX <mouna1.saiss@gmail.com>
2025-09-28 10:55:01 +0100

    fichichier main

---
+++ mini_project/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"header.h"
+#include"Fuctions.c"
+
+
+
+
+
+int main (){
+InitialiserAvions();
+afficheMenu();
+    return 0;
+}                                                          
